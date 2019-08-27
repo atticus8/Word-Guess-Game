e69ab625e3f4f078a0f9c244697cc2c7c0928660
atticus8 <atticus8@gmail.com>
2019-08-27 12:07:50 -0400

    new layout

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+
+    var library = ["Alpha","Beta","C","Delta"];
+    var lives = 7;
+    var guessedLetters = [];
+    var currentWordIndex;
+    var thisWord = [];
+    var remainingGuesses = 0;
+    var start = false;
+    var finish = false;
+
+    function 
+    
+    
+    
+
+
+
